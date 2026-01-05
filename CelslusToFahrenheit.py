c9264e9fc9a47851b217b713ccd93cddb59cda69
Prasad Shinde <prasadshinde150904@gmail.com>
2026-01-05 11:09:54 +0530

    Add files via upload

--- CelslusToFahrenheit.py
+++ CelslusToFahrenheit.py
@@ -1,4 +1,4 @@
 celsius=float(input("Enter temperature in Celsius:"))
 #formula
 fahrenheit=(celsius*9/5)+12
-print(f"{celsius} degree celsies is equal to {fahrenheit} degree fahrenheit")
+print(f"{celsius} degree celsies is equal to {fahrenheit} degree fahrenheit")                             
