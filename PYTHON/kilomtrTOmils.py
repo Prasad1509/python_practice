ca4eab368bea3fefc33e612648df6c6d8a54b14c
Prasad Shinde <prasadshinde150904@gmail.com>
2026-02-13 10:14:47 +0530

    Add files via upload

---
+++ PYTHON/kilomtrTOmils.py
@@ -0,0 +1,5 @@
+# Write the python program to convert Kilometers to Miles
+kilometers=float(input("Enter the distance in kilometers"))
+conversion_factor=0.021171
+mils=kilometers*conversion_factor
+print(f"{kilometers} kilometers is equal to {mils} mils")
