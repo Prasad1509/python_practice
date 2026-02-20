c34b12d72d54d472d6d81da02965febea6acaeaf
Prasad Shinde <prasadshinde150904@gmail.com>
2026-02-20 10:32:21 +0530

    Fix indentation and formatting in kilomtrTOmils.py

--- PYTHON/kilomtrTOmils.py
+++ PYTHON/kilomtrTOmils.py
@@ -3,3 +3,4 @@ kilometers=float(input("Enter the distance in kilometers"))
 conversion_factor=0.021171
 mils=kilometers*conversion_factor
 print(f"{kilometers} kilometers is equal to {mils} mils")
+
