f18640a37e60cd7b66114b8d7cb5e982ab7faee8
Prasad Shinde <prasadshinde150904@gmail.com>
2026-03-29 21:10:51 +0530

    Format leap year check code

--- PYTHON/leap.py
+++ PYTHON/leap.py
@@ -2,7 +2,7 @@ year = int(input("Enter a year: "))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0) : 
     print(year, "is a leap year")
-else:         
+else:          
     print(year, "is not a leap year")
     
   
