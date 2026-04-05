161492e84486a38b63de667c046ea90a01be838b
Prasad Shinde <prasadshinde150904@gmail.com>
2026-04-05 22:16:18 +0530

    Fix indentation and formatting in Diamond.py

--- HomeWork/Diamond.py
+++ HomeWork/Diamond.py
@@ -4,6 +4,6 @@ n = 5
 for i in range(1, n + 1):
     print(" " * (n - i) + "*" * (2 * i - 1))
 
-# Lower Half
+# Lower Half 
 for i in range(n - 1, 0, -1):
     print(" " * (n - i) + "*" * (2 * i - 1))
