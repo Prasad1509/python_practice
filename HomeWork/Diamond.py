becbbf983210989afefbbd87b62f46f4bbfe04da
Prasad Shinde <prasadshinde150904@gmail.com>
2026-05-08 15:57:44 +0530

    Reformat Diamond.py for consistent indentation

--- HomeWork/Diamond.py
+++ HomeWork/Diamond.py
@@ -5,5 +5,5 @@ for i in range(1, n + 1):
     print(" " * (n - i) + "*" * (2 * i - 1))
 
 # Lower Half 
-for i in range(n - 1, 0, -1):
+for i in range(n - 1, 0, -1): 
     print(" " * (n - i) + "*" * (2 * i - 1))
