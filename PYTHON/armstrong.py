4f603fb318294380c2c904d8186d71f77a753a34
Prasad Shinde <prasadshinde150904@gmail.com>
2026-06-09 16:14:59 +0530

    Update armstrong.py

--- PYTHON/armstrong.py
+++ PYTHON/armstrong.py
@@ -8,7 +8,7 @@ while temp > 0:
     temp //= 10
 
 if sum == num:
-    print(f"{num} is an Armstrong number")
+    print(f"{num} is an Armstrong number") 
 else:
     print(f"{num} is not an Armstrong number")
 
