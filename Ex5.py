407b75353a3482268f6927f9d9045b7f76cb6b5b
Prasad Shinde <prasadshinde150904@gmail.com>
2026-06-16 21:51:57 +0530

    Update Ex5.py

--- Ex5.py
+++ Ex5.py
@@ -2,7 +2,7 @@
 try:
     num1=int(input("Enter First Number:"))
     num2=int(input("Enter Second Number:"))
-
+ 
     result=num1/num2
     print("Result:",result)
 except (ZeroDivisionError,ValueError):
